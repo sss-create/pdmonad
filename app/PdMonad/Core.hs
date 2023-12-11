4c3c39202552b3d03f44d18763e465cb01e6d4be
sss-create <sven.miesner@tutanota.com>
2023-12-11 18:47:32 +0100

    split functions for readability

--- app/PdMonad/Core.hs
+++ app/PdMonad/Core.hs
@@ -17,69 +17,72 @@ import Data.Text.IO qualified as T
 -- | type creations
 
 data PdObject = PdObject
-  { objectId :: Maybe Int,
-    objectType :: T.Text,
-    objectCoordinates :: (Double, Double),
-    objectArguments :: T.Text
-  }
-  deriving (Show, Eq)
+  { objectId          :: Maybe Int
+  , objectType        :: T.Text
+  , objectCoordinates :: (Double, Double)
+  , objectArguments   :: T.Text
+  } deriving (Show, Eq)
 
 
 data PdConnection = PdConn
-  { source :: Maybe Int,
-    outlet :: Int,
-    sink :: Maybe Int,
-    inlet :: Int
-  }
-  deriving (Show)
+  { source :: Maybe Int
+  , outlet :: Int
+  , sink   :: Maybe Int
+  , inlet  :: Int
+  } deriving (Show)
 
 
 data PdPatch = PdPatch
-  { pdObjects :: [PdObject],
-    pdConnections :: [PdConnection]
-  }
-  deriving (Show)
+  { pdObjects     :: [PdObject]
+  , pdConnections :: [PdConnection]
+  } deriving (Show)
+
+
+graphAttributes :: [GlobalAttributes]
+graphAttributes = [GraphAttrs [Layout Dot, RankDir FromBottom, NodeSep 1, RankSep [0.02]]]
 
 
 ------------------------------------------------------------------------
 -- | patch synthesis
 
-writePatch :: String -> [PdPatch] -> IO ()
+writePatch :: String -> PdPatch -> IO ()
 writePatch name pdPatch = do
   placedPatch <- placement pdPatch
-  let textObjects = concatMap (map objectToText . pdObjects) placedPatch
-      textConnections = concatMap (map connectionToText . pdConnections) pdPatch
+  let textObjects = map objectToText . pdObjects $ placedPatch
+      textConnections = map connectionToText . pdConnections $ pdPatch
       canvas = [T.pack "#N canvas 0 50 450 300 12;"]
   T.writeFile name $ T.unlines $ concat [canvas, textObjects, textConnections]
 
 
-placement :: [PdPatch] -> IO [PdPatch]
+placement :: PdPatch -> IO PdPatch
 placement pdPatch = do
-  let numberedObjects = nub . concatMap pdObjects $ pdPatch
+  placedObjects <- getCoordinatesFromGraph $ patchToGraph pdPatch
+  let zippedCoords = zipWith (\x j -> j {objectCoordinates = x}) placedObjects $ pdObjects pdPatch
+  pure . PdPatch zippedCoords $ pdConnections pdPatch
+
+
+preparePatch :: [PdPatch] -> PdPatch
+preparePatch pdPatch =
+  let uniqueObjs = nub . concatMap pdObjects $ pdPatch
       connections = concatMap pdConnections pdPatch
-  placedObjects <- getCoordinatesFromGraph $ patchToGraph numberedObjects connections
-  let zippedCoords = zipWith (\x j -> j {objectCoordinates = x}) placedObjects numberedObjects
-  return [PdPatch zippedCoords (concatMap pdConnections pdPatch)]
+  in PdPatch uniqueObjs connections
 
 
 objectToText :: PdObject -> T.Text
 objectToText object =
   let (x, y) = objectCoordinates object
       fields = [objectType object, T.pack $ show x, T.pack $ show y, objectArguments object]
-   in T.append (T.unwords fields) ";"
+  in T.append (T.unwords fields) ";"
 
 
 connectionToText :: PdConnection -> T.Text
-connectionToText connection =
-  T.pack $
-    unwords
-      [ "#X connect",
-        show (fromMaybe 0 $ source connection),
-        show (outlet connection),
-        show (fromMaybe 0 $ sink connection),
-        show (inlet connection)
-      ]
-      ++ ";"
+connectionToText connection = T.pack $ unwords
+  [ "#X connect"
+  , show (fromMaybe 0 $ source connection)
+  , show (outlet connection)
+  , show (fromMaybe 0 $ sink connection)
+  , show (inlet connection)
+  ] ++ ";"
 
 
 ------------------------------------------------------------------------
@@ -89,11 +92,10 @@ infixl 0 -->
 (-->) :: PdPatch -> PdObject -> PdPatch
 PdPatch objects connections --> nextObject =
   let newObjectList = objects ++ [nextObject]
-      newConnections =
-        case objects of
-          [] -> connections
-          _ -> connections ++ [PdConn (objectId $ last objects) 0 (objectId nextObject) 0]
-   in PdPatch newObjectList newConnections
+      newConnections = case objects of
+        [] -> connections
+        _ -> connections ++ [PdConn (objectId $ last objects) 0 (objectId nextObject) 0]
+  in PdPatch newObjectList newConnections
 
 
 infixr 1 #
@@ -104,11 +106,11 @@ object # n = object {objectId = Just n}
 ------------------------------------------------------------------------
 -- | graph methods
 
-patchToGraph :: [PdObject] -> [PdConnection] -> Gr Int Int
-patchToGraph indexedObjects connections =
-  let nodes = fmap ((\i -> (i, i)) . fromMaybe 0 . objectId) indexedObjects
-      edges = [(fromMaybe 0 i, fromMaybe 0 j, 0) | PdConn i _ j _ <- connections]
-   in mkGraph nodes edges
+patchToGraph :: PdPatch -> Gr Int Int
+patchToGraph p =
+  let nodes = map ((\i -> (i,i)) . fromMaybe 0 . objectId) $ pdObjects p 
+      edges = [(fromMaybe 0 i, fromMaybe 0 j, 0) | PdConn i _ j _ <- pdConnections p]
+  in mkGraph nodes edges
 
 
 -- | labNodes :: gr a b -> [LNode a]
@@ -118,10 +120,10 @@ patchToGraph indexedObjects connections =
 -- in [Attributes]
 getCoordinatesFromGraph :: Gr Int Int -> IO [(Double, Double)]
 getCoordinatesFromGraph gr = do
-  layoutedGraph <- graphToGraph nonClusteredParams {globalAttributes = ga} gr
-  return . map getXY . nodeToCoords $ layoutedGraph
-  where
-    nodeToCoords = concatMap (fst . snd) . labNodes
-    ga = [GraphAttrs [Layout Dot, RankDir FromBottom, NodeSep 1, RankSep [0.02]]]
-    getXY :: Attribute -> (Double, Double)
-    getXY (Pos (PointPos (Point {xCoord = x, yCoord = y}))) = (x, y)
+  layoutedGraph <- graphToGraph nonClusteredParams {globalAttributes = graphAttributes} gr
+  let nodeToCoords = concatMap (fst . snd) . labNodes
+  pure . map xyCoordinates . nodeToCoords $ layoutedGraph
+
+
+xyCoordinates :: Attribute -> (Double, Double)
+xyCoordinates (Pos (PointPos (Point {xCoord = x, yCoord = y}))) = (x, y)
--- app/PdMonad/Main.hs
+++ app/PdMonad/Main.hs
@@ -11,13 +11,13 @@ variable = msg "$1" # 1
 hworld = msg "hello, world"
 three = msg "3"
 
+start = PdPatch [] []
 
 main :: IO ()
-main =  writePatch "mypatch.pd"
-    [
-        PdPatch [] [] --> number --> variable --> obj "print" # 2,
-        PdPatch [] [] --> lbang --> hworld # 4 --> obj "print" # 5,
-        PdPatch [] [] --> lbang --> three # 6 --> number,
-        PdPatch [] [] --> 
-            obj "osc~ 220" # 7 --> obj "*~ 1" # 8 --> obj "cyclone/snapshot~ 50" # 9 --> num
+main =  writePatch "mypatch.pd" $ preparePatch
+    [ 
+      start --> number --> variable --> obj "print" # 2,
+      start --> lbang --> hworld # 4 --> obj "print" # 5,
+      start --> lbang --> three # 6 --> number,
+      start --> obj "osc~ 220" # 7 --> obj "*~ 1" # 8 --> obj "cyclone/snapshot~ 50" # 9 --> num
     ]
